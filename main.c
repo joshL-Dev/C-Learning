ddddadc86a607bd5defa666de269dc2d1d3a7b1d
joshL-Dev <70299357+joshL-Dev@users.noreply.github.com>
2022-05-13 01:00:32 -0600

    First C file

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+Author: Josh L
+Purpose:    Doin' me some learnin'
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+    // this code is displaying my name
+    printf("Hi, my name is Josh");
+    return 0;
+}
