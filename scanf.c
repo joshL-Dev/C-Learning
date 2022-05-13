c86b6fe6ce4abee4e09f40f611312dacb3510a30
joshL-Dev <70299357+joshL-Dev@users.noreply.github.com>
2022-05-13 01:18:16 -0600

    scanf

---
+++ scanf.c
@@ -0,0 +1,20 @@
+/*
+Author: Josh L
+Purpose:    Doin' me some learnin'
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+    char str [100];
+    int i;
+
+    printf("Enter a Value:");
+
+    scanf ("%d", &i);
+
+    printf("\nYou Entered: %d\n", i);
+
+    return 0;
+}
